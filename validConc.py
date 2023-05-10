763088d305e014665e637bb80d4d04175630d860
navinRuas <navinchandry.ruas@inep.gov.br>
2023-05-10 15:00:34 -0300

    desisto

--- validConc.py
+++ validConc.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
-from emailFunc import enviar_notificacao, enviar_notificacao_supervisor, personalizar_html
+from emailFunc import enviar_notificacao, enviar_notificacao_supervisor
+from extraUtils import personalizar_html, gap
 from Conexao import pontalina
 
 def validar_conclusao_plano_trabalho():
@@ -16,12 +17,12 @@ def validar_conclusao_plano_trabalho():
         servidores[servidor].append(dado)
     
     # Carregar lista de servidores não concluídos
-    with open('nConc.json', 'r') as f:
+    with open(gap('data\\nConc.json'), 'r') as f:
         nConc = json.load(f)
     
     # 3. Para cada servidor:
     for servidor, atividades in servidores.items():
-        todas_concluidas = all(atividade['SituacaoPactoTrabalho'] == 'Concluída' for atividade in atividades)
+        todas_concluidas = all(atividade['SituacaoAtividade'] == 'Concluída' for atividade in atividades)
         
         # 3.1. Se todas as atividades com o mesmo pactoTrabalhoId tiverem status Concluída, ignorar e passar para o próximo servidor.
         if todas_concluidas:
@@ -33,14 +34,12 @@ def validar_conclusao_plano_trabalho():
         
         # 3.2. Se a data DtFimPactoTrab estiver faltando 1 dia para vencer, enviar uma notificação ao servidor usando o arquivo HTML avisoConc1.html.
         if dias_restantes == 1:
-            html_personalizado = personalizar_html('avisoConc1.html', {'NomeServidor': servidor})
-            enviar_notificacao(servidor, html_personalizado)
+            enviar_notificacao(servidor, personalizar_html(gap('mail\\avisoConc1.html'), {'nome': servidor, 'data' : datetime.now().strftime('%d/%m/%Y')}))
         
         # 3.3. Se a data DtFimPactoTrab já estiver vencida, enviar uma notificação ao servidor e ao supervisor usando o arquivo HTML avisoNConc.html.
         elif dias_restantes < 0:
-            html_personalizado = personalizar_html('avisoNConc.html', {'NomeServidor': servidor})
-            enviar_notificacao(servidor, html_personalizado)
-            enviar_notificacao_supervisor(servidor, html_personalizado)
+            enviar_notificacao(servidor, personalizar_html(gap('mail\\avisoNConc.html'), {'nome': servidor, 'data' : datetime.now().strftime('%d/%m/%Y')}))
+            enviar_notificacao_supervisor(servidor, personalizar_html(gap('mail\\avisoNConc.html'), {'nome': servidor}))
             
             # 3.4. Adicionar o servidor à lista de servidores não concluídos em um arquivo nConc.json.
             if servidor not in nConc:
@@ -49,10 +48,13 @@ def validar_conclusao_plano_trabalho():
     # 3.5. Verificar se os servidores na lista já concluíram as atividades. Caso tenham concluído, remover da lista.
     for servidor in list(nConc.keys()):
         atividades = servidores.get(servidor, [])
-        todas_concluidas = all(atividade['SituacaoPactoTrabalho'] == 'Concluída' for atividade in atividades)
+        todas_concluidas = all(atividade['SituacaoAtividade'] == 'Concluída' for atividade in atividades)
         if todas_concluidas:
             del nConc[servidor]
     
     # Salvar lista atualizada de servidores não concluídos
-    with open('nConc.json', 'w') as f:
+    with open(gap('data\\nConc.json'), 'w') as f:
         json.dump(nConc, f)
+
+if __name__ == '__main__':
+    validar_conclusao_plano_trabalho()
